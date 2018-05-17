ff3b567bb336cf8bd97600a99f23f01158aa7d96
JooYoo <jooyoo@outlook.com>
2018-05-17 15:57:41 +0200

    added right side drawer

---
+++ SideDrawer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace SideDrawer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRightMenuHide_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbHideRightMenu", btnRightMenuHide, btnRightMenuShow, pnlRightMenu);
+        }
+        private void btnRightMenuShow_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbShowRightMenu", btnRightMenuHide, btnRightMenuShow, pnlRightMenu);
+        }
+        private void ShowHideMenu(string Storyboard, Button btnHide, Button btnShow, StackPanel pnl)
+        {
+            Storyboard sb = Resources[Storyboard] as Storyboard;
+            sb.Begin(pnl);
+
+            if (Storyboard.Contains("Show"))
+            {
+                btnHide.Visibility = Visibility.Visible;
+                //btnShow.Visibility = Visibility.Hidden;
+            }
+            else if (Storyboard.Contains("Hide"))
+            {
+                //btnHide.Visibility = Visibility.Hidden;
+                btnShow.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
